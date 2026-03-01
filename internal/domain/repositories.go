2358ede2c171aff3a0538b1874165e88bef2e929
Flarenzy <>
2026-03-01 10:22:00 +0100

    add repositories contracts in internal domain

---
+++ internal/domain/repositories.go
@@ -0,0 +1,18 @@
+package domain
+
+import "context"
+
+type SubnetRepository interface {
+	List(ctx context.Context) ([]Subnet, error)
+	FindByID(ctx context.Context, id int64) (Subnet, error)
+	Create(ctx context.Context, input CreateSubnetInput) (Subnet, error)
+	Delete(ctx context.Context, id int64) (bool, error)
+}
+
+type IPRepository interface {
+	ListBySubnetID(ctx context.Context, subnetID int64) ([]IPAddress, error)
+	FindByIDAndSubnet(ctx context.Context, id string, subnetID int64) (IPAddress, error)
+	Create(ctx context.Context, input CreateIPInput, subnetID int64) (IPAddress, error)
+	UpdateHostname(ctx context.Context, id string, input UpdateIPInput) (IPAddress, error)
+	DeleteByIDAndSubnet(ctx context.Context, id string, subnetID int64) (bool, error)
+}
